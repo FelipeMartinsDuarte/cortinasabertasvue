4a6015a72b3b8a359ec9254a272569c3f8781b3b
felipe <felipemartinsduarte@outlook.com.br>
2021-12-24 01:05:18 -0300

    master

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu/>
+    <Backbutton />
+    <Title />
+    <InputRegister />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/menu.vue'
+import Backbutton from './components/backbutton.vue'
+import Title from './components/title.vue'
+import InputRegister from './components/InputRegister.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu,
+    Backbutton,
+    Title,
+    InputRegister
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+                                            
 </style>
---
+++ src/components/InputRegister.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="Form">
+    <div class="wrap">
+      <form id="registrar" method="post" action="/register/password">
+        <input name="name" type="hidden" :value="name" />
+        <input name="email" type="hidden" :value="email" />
+
+        <label for="password">CRIAR UMA SENHA</label><br />
+        <input name="password" type="password" /><br /><br />
+
+        <label for="confirmpassword">CONFIRMAR SENHA</label><br />
+        <input name="confirmpassword" type="password" /><br /><br />
+
+        <input type="submit" value="Criar Conta" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputRegister",
+};
+</script>
+
+<style scoped>
+
+.Form{
+    display: grid;
+    grid-template-columns: 37px 74px 74px 74px 74px 74px 74px ;
+    grid-gap: 10px;
+}
+
+.wrap {
+  grid-column-start:2;
+  width: 100vw;
+  height: 36px;
+}
+
+input[type=password]{
+ font-weight: 200;
+ font-size: 16px;
+ color: #707070;
+ outline: none;
+ border: 2px solid rgb(185,185,185);
+ border: 2px solid rgba(185,185,185,.5);
+ -webkit-background-clip: padding-box; /* for Safari */
+ background-clip: padding-box; /* for IE9+, Firefox 4+, Opera, Chrome */
+ border-radius: 5px;
+ height: 52px;
+ width: 296px;
+}
+
+input[type=password]:focus{
+ color: #0D0D0D;
+ border: 2px solid #16D9F2;
+}
+
+input[type=submit]{
+ font-weight: 600;
+ font-size: 16px;
+ border: none;
+ color: white;
+ background-color:#68B400;
+ width: 315px;
+ height: 52px;
+ border-radius: 5px;
+}
+
+label{
+    font-weight: 400;
+    display: block;
+    font-size: 16px;
+}
+
+
+</style>
