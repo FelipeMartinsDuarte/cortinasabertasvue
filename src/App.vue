0c43a9441dc579af30189c229974b5edb502ee61
FelipeMartinsDuarte <69360656+FelipeMartinsDuarte@users.noreply.github.com>
2021-12-27 19:38:31 -0300

    menu

--- src/App.vue
+++ src/App.vue
@@ -1,185 +1,58 @@
 <template>
   <div id="app">
-    <header>
-      <div class="menu">
-        <ul>
-          <li><a href="#"><img src="./assets/logo.svg" alt="logomarca cortinas abertas" /></a></li>
-        </ul>
-        
-        <ul class="nav">
-          <li><a href="#">Anuncie seu Lar</a></li>
-          <li><a href="#">Cadastrar-se</a></li>
-          <li><a href="#">Entrar</a></li>
-        </ul>
+    <Menu />
+    <Breakline />
 
-        <ul class="page-information">
-          <li>Cadastro</li>
-          <li>Identificação</li>
-        </ul>
+    <div id="content-wrap">
 
+      <div id="backbutton">
+        <img src="./assets/backarrow.svg" alt="botão voltar" />
+        <span>Voltar</span>
+      </div>
 
-        </div>
-    </header>
-   <div id="breakline"></div>
-    <div id="backbutton">
-      <img src="./assets/backarrow.svg" alt="botão voltar" />
-      <span>Voltar</span>
+      <div class="title">
+        <h1>CADASTRAR</h1>
+      </div>
+
     </div>
-
-    <div class="title">
-      <h1>CADASTRAR</h1>
-    </div>
-
   </div>
 </template>
 
 <script>
+import Menu from "./components/menu.vue";
+import Breakline from "./components/breakline.vue";
+
+
 export default {
-  name: "App", //,
-  //components: {
-  //},
+  name: "App",
+  components: {
+    Menu,
+    Breakline
+  },
 };
 </script>
 
 <style>
-/*Grid*/
 
-#app{
+/*Grid*/
+#content-wrap {
   display: grid;
-  grid-template-columns: 21% repeat(12,1fr) 21%;
+  grid-template-columns: 21% repeat(12, 1fr) 21%;
   gap: 20px;
 }
 
-header{
-  grid-column: 2/14;
-}
-
-#backbutton{
+#backbutton {
   grid-column: 2/15;
 }
 
-.title{
+.title {
   grid-column: 5/15;
 }
 
-#breakline{
-  grid-column: 1/15;
-}
-
-/* Menu */
-
-/*Breakline*/
-#breakline{
-  -webkit-box-shadow:0px 3px 6px #8a8a8a7c;
- -moz-box-shadow:0px 3px 6px #8a8a8a7c;
- box-shadow:0px 3px 6px #8a8a8a7c;
-  border-bottom: 1px solid transparent;
-}
-
-/* Set main Property*/
-
-.menu{
-  padding-top: 16px;
-}
-
-.menu, ul {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-content: center;
-}
-
-/* Set Padding */
-
-.menu ul:nth-child(3n) {
-  padding-top: 16px;
-  width: 50%;
-}
-
-.menu ul:nth-child(3n) li{
-  padding-right: 16px;
-}
-
-/* Set basic property */
-
-
-.menu ul li a{
-  display: block;
-  width: 100%;
-  height: 100%;
-  text-decoration: none;
-}
-
-.menu ul[class="page-information"] li, ul[class="nav"] li {
-  font-weight: 400;
-  font-size: 12px;
-  padding: 6px;
-  background-color: #00A28C;
-  border-radius: 5px;
-  margin-left: 8px;
-  margin-right: 8px;
-  width: 7vmax;
-  text-align: center;
-}
-
-/* Set Page information property */
-
-.menu ul[class="page-information"] li{
-  width: auto;
-  font-weight: 100;
-  background-color: #f2eeeb48;
-  border-radius: 7px;
-  margin-left: inherit;
-  padding: 4px 32px 4px 32px;
-
-}
-
-.menu ul[class="nav"] li:nth-child(1){
-  width: 8.5vmax;
-  background-color: inherit;
-  border: 1px solid #00A28C;
-}
-
-
-/* Set font colors */
-
-.menu ul[class="nav"] a{
-  color: white;
-}
-
-.menu ul[class="nav"] li:nth-child(1) a {
-  color: #00A28C;
-}
-
-/* Hover */
-
-.menu ul[class="nav"] li:nth-child(2):hover{
-  background-color: #019b86;
-}
-
-.menu ul[class="nav"] li:nth-child(3):hover{
-  background-color: #019b86;
-}
-
-
-.menu, ul:nth-child(2n){
-  justify-content: space-between;
-}
-
-/*Adjust items */
-
-.menu img {
-  width: auto;
-  height: 16px;
-}
-
-
-
 /* Back button */
-#backbutton{
+#backbutton {
   padding-bottom: 32px;
 }
-
 
 #backbutton span {
   font-weight: 600;
@@ -188,7 +61,7 @@ header{
   font-size: 12px;
 }
 
-#backbutton span:hover{
+#backbutton span:hover {
   color: #00584d;
   cursor: pointer;
 }
@@ -202,8 +75,5 @@ header{
   justify-content: center;
   text-align: center;
 }
-
-
-
 
 </style>
