7e77f5f75ace5fe21f80329f5e70cb9c703694a7
FelipeMartinsDuarte <69360656+FelipeMartinsDuarte@users.noreply.github.com>
2021-12-27 19:43:48 -0300

    menu

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,10 @@
   <div id="app">
     <Menu />
     <Breakline />
+    <Backbutton />
 
     <div id="content-wrap">
 
-      <div id="backbutton">
-        <img src="./assets/backarrow.svg" alt="botão voltar" />
-        <span>Voltar</span>
-      </div>
 
       <div class="title">
         <h1>CADASTRAR</h1>
@@ -21,19 +18,19 @@
 <script>
 import Menu from "./components/menu.vue";
 import Breakline from "./components/breakline.vue";
-
+import Backbutton from "./components/backbutton.vue";
 
 export default {
   name: "App",
   components: {
     Menu,
-    Breakline
+    Breakline,
+    Backbutton
   },
 };
 </script>
 
 <style>
-
 /*Grid*/
 #content-wrap {
   display: grid;
@@ -41,33 +38,11 @@ export default {
   gap: 20px;
 }
 
-#backbutton {
-  grid-column: 2/15;
-}
-
 .title {
   grid-column: 5/15;
 }
 
-/* Back button */
-#backbutton {
-  padding-bottom: 32px;
-}
-
-#backbutton span {
-  font-weight: 600;
-  padding-left: 8px;
-  color: #006154;
-  font-size: 12px;
-}
-
-#backbutton span:hover {
-  color: #00584d;
-  cursor: pointer;
-}
-
 /*Tittle Description */
-
 .title {
   width: 26vmax;
   display: flex;
--- src/components/backbutton.vue
+++ src/components/backbutton.vue
@@ -1,5 +1,42 @@
 <template>
     <div class="wrap">
-        
+        <div id="backbutton">
+        <img src="../assets/backarrow.svg" alt="botão voltar" />
+        <span>Voltar</span>
+      </div>
     </div>
 </template>
+
+<script>
+export default {
+    name:"backbutton"
+}
+</script>
+
+<style scoped>
+.wrap {
+  display: grid;
+  grid-template-columns: 21% repeat(12, 1fr) 21%;
+  gap: 20px;
+}
+
+#backbutton {
+  grid-column: 2/15;
+}
+
+#backbutton {
+  padding-bottom: 32px;
+}
+
+#backbutton span {
+  font-weight: 600;
+  padding-left: 8px;
+  color: #006154;
+  font-size: 12px;
+}
+
+#backbutton span:hover {
+  color: #00584d;
+  cursor: pointer;
+}
+</style>
