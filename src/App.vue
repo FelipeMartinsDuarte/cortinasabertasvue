e9a5affb61c81df6a506bb5f794280c38ac7636f
FelipeMartinsDuarte <69360656+FelipeMartinsDuarte@users.noreply.github.com>
2021-12-29 04:43:05 -0300

    verificandoperfil

--- src/App.vue
+++ src/App.vue
@@ -141,8 +141,36 @@
           <a href="#">Continuar</a>
           <input type="reset" value="Cancelar" />
         </form>
-      </div>
 
+
+      </div>
+      <div class="account-details">
+          <div class="top-footer">
+            <ul>
+              <li><a href="#">Sua Conta</a></li>
+              <li><a href="#">Contate-nos</a></li>
+            </ul>
+          </div>
+          </div>
+
+          <footer>
+          <div class="button-footer">
+            <ul>
+              <li><a href="#">Sobre nós</a></li>
+              <li><a href="#">Bairros</a></li>
+              <li><a href="#">Politica</a></li>
+              <li><a href="#">Contate-nos</a></li>
+              <li><a href="#">Ajuda</a></li>
+              <li><a href="#">Regiões</a></li>
+              <li><a href="#">Privacidade</a></li>
+              <li><a href="#">Pagamentos</a></li>
+              <li><a href="#">Favoritos</a></li>
+              <li><a href="#">Notificação</a></li>
+              <li><a href="#">Configurações</a></li>
+              <li><a href="#">Dúvidas</a></li>
+            </ul>
+          </div>
+        </footer>
     </div>
   </div>
 </template>
@@ -201,7 +229,65 @@ export default {
 .card-endereco {
   grid-column: 2/13;
 }
+.account-details{
+  grid-column: 1/16;
+}
+footer{
+  grid-column: 1/15;
+}
 
+/*Footer */
+footer{
+  margin: -20px;
+  padding: 16px;
+  background-color: #0D0D0D;
+  display: grid;
+  grid-template-columns: 21% repeat(12, 1fr) 21%;
+  gap: 20px;
+}
+
+.account-details{
+  padding: 16px 0px;
+  background-color: #E1E3E2;
+  display: grid;
+  grid-template-columns: 21% repeat(12, 1fr) 21%;
+  gap: 20px;
+}
+
+.account-details .top-footer{
+  grid-column: 2/15;
+}
+
+footer .button-footer{
+  grid-column: 2/15;
+}
+
+footer .button-footer ul, .account-details .top-footer ul {
+  width: 24vmax;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+footer .button-footer ul li, .account-details .top-footer ul li {
+  margin-right: 16px;
+}
+
+
+footer .button-footer ul li{
+  margin-top: 8px;
+}
+
+footer a{
+  color: white;
+  font-weight: 200;
+} 
+
+.top-footer a {
+  color: #006154;
+  font-weight: 200;
+
+}
 /*Card-Name*/
 .card-name input[type="text"],
 .card-cnpj input[type="text"],
