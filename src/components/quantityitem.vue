5d1b7ab656e130e485009b6633e8cf6e3526ce4f
FelipeMartinsDuarte <69360656+FelipeMartinsDuarte@users.noreply.github.com>
2022-01-09 15:09:30 -0300

    cardteamstructure

---
+++ src/components/quantityitem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+         <img v-bind:src="'data:image/png;base64,'+Base64">
+         <span>{{name}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"quantityitem",
+    props:{
+        name: String,
+        type: String,
+        Base64: String,
+    }
+}
+</script>
+
+<style scoped>
+
+
+div{
+    width: 65%;
+    font-weight: 100;
+    color: #006154;
+    display: flex;
+    flex-direction: row;
+    font-weight: 100;
+    align-items: center;
+}
+
+div img{
+    margin-bottom: 4px;
+}
+
+</style>
