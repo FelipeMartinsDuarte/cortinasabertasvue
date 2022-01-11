478b619ed82fcb7664a57d960635973d0a415a12
FelipeMartinsDuarte <69360656+FelipeMartinsDuarte@users.noreply.github.com>
2022-01-11 20:01:20 -0300

    imagem

--- src/App.vue
+++ src/App.vue
@@ -15,25 +15,29 @@
             Coloque uma ou mais Imagens sobre a estrutura qual queira mostrar 
             ao visitante, com no mínimo 640x360 Pixels
           </p>
-          <div id="image-content">
-            <div class="image-preview" v-if="imagelenght > 0">
-              
-              <div v-for="(item,index) in imageArray" :key="index">
-                <div class="imgs">
-                <div class="meta-data">
-                <img src="./assets/bin.svg" alt="remove image" @click="removeImage(item)">
-                <span>{{item.filename}}</span>
-                </div>
-                <img :src="item.url">
+          <div id="image-content" @dragover.prevent @drop.stop.prevent="ondrop">
+
+            <div class="image-preview" >
+                <div class="border add">
+                  <img src="./assets/imageicon.svg"/>
+                  <span><a href="#" v-on:click="imageSelect">Clique e selecione</a> ou Arraste aqui</span>
                 </div>
               </div>
-            </div>
-            <div class="add-image" v-if="imagelenght == 0">
-              <img src="./assets/imageicon.svg"/>
-              <span><a href="#" v-on:click="imageSelect">Clique e selecione</a> ou Arraste aqui uma ou mais Imagens</span>
-            </div>
 
-          <input type="file" ref="imageInput" @input="verificateImage" multiple>
+              <div v-for="(item,index) in imageArray" :key="index" class="image-preview">
+                  <div id="top-bar">
+                    <img src="./assets/bin.svg" alt="remove image" @click="removeImage(item)">
+                    <span>{{item.filename}}</span>
+                  </div>
+                   
+                  <div class="border image">
+                    <img :src="item.url">
+                  </div>
+              </div>
+
+              
+
+          <input type="file" ref="imageInput" @input="onselect" multiple>
           </div>
 
 
@@ -49,7 +53,30 @@
             Coloque a sua logo em fundo transparente ou branco, caso não possua uma ou não esteja 
             com o arquivo basta avançar para próxima etapa, você poderá adicionar futuramente
           </p>
+          <div id="logo-content">
+            <div class="preview-logo" v-for="(item,index) in logoItem" :key="index">
+              <img :src="item.url">
+            </div>
+            <div class="add-logo" @click="logoSelect" v-if="logolenght == 0">
+            <img src="./assets/logoicon.svg">
+            </div>
+
+            <div class="row navlogo" v-if="logolenght > 0">
+              <div class="edit">
+                <img src="./assets/crop.svg">
+              </div>
+              <div class="remove" @click="removeLogo">
+                <img src="./assets/bin.svg">
+              </div>
+            </div>
+
+            <input type="file" ref="logoInput" @input="onlogoselect">
+          </div>
+  
+
         </div>
+
+  
 
         <form class="sendreset">
           <hr />
@@ -60,12 +87,15 @@
       </div>
 
     </div>
+    
   </div>
+  
 </template>
 
 <script>
 import Menu from "./components/menu.vue";
 import Breakline from "./components/breakline.vue";
+import Cropper from 'cropperjs';
 
 export default {
   name: "App",
@@ -78,9 +108,26 @@ export default {
       imageArray:[],
       imageItem:[],
       imagelenght:0,
+      logoItem:[],
+      logolenght:0,
     };
   },
   methods:{
+    ondrop: function(event){
+        let limit = 13;
+        let Images = [...event.dataTransfer.files];
+        let cbv = Images.length + this.imagelenght;
+        let oquesobrou = limit - this.imagelenght;
+        console.log(oquesobrou);
+        if(cbv < limit){
+        this.verificateImage(Images);
+        }
+    },
+    onselect: function(){
+      const imageInput = this.$refs.imageInput;
+      let Images = [...imageInput.files];
+      this.verificateImage(Images);
+    },
     removeImage: function(item){
       var position = this.imageItem.indexOf(item.filename);
       this.imagelenght -= 1;
@@ -90,13 +137,10 @@ export default {
     imageSelect: function (){
       this.$refs.imageInput.click()
     },
-    verificateImage: function(){
+    verificateImage: function(Images){
       //Define rules
       const av = ["image/jpeg","image/jpg","image/png","image/webp"];
       const maxsize = 3000000; //3Mb to Bytes
-      //Import Files
-      const imageInput = this.$refs.imageInput;
-      let Images = [...imageInput.files];
       //Check item by item comply with the rule
       for(var item of Images){
         let url = URL.createObjectURL(item);
@@ -110,6 +154,7 @@ export default {
         }
         if(!rsize){
           console.log("Arquivo muito grande envie no máximo 3mb")
+          continue;
         }
         this.previewImage(item, url);
       }
@@ -122,6 +167,34 @@ export default {
         this.imageArray.push(imageObj);
         this.imageItem.push(imageObj.filename);
         this.imagelenght +=1;
+        console.log(this.logoItem)
+      },
+
+      //Image End
+      //Logo 
+
+      logoSelect: function (){
+      this.$refs.logoInput.click()
+    },
+
+      onlogoselect: function(){
+        const logoInput = this.$refs.logoInput;
+        let Logo = logoInput.files[0];
+        let url = URL.createObjectURL(Logo);
+        this.onlogopreview(Logo,url);
+      },
+      onlogopreview: function(item,url){
+        console.log(url);
+        let Logo = new Object;
+        Logo.url = url;
+        Logo.filename = item.name;
+        Logo.filetype = item.type;
+        this.logoItem.push(Logo);
+        this.logolenght +=1;
+      },
+      removeLogo: function(){
+        this.logolenght = 0;
+        this.logoItem.splice(0,1);
       }
 
     }
@@ -147,72 +220,152 @@ export default {
 
 /*Image-Card*/
 
-/*Image-content */
-#image-content{
-  border: 1px solid black;
-  width: 100%;
-  height: 12.8vmax;
+/*Logo*/
+.row{
+  display: flex;
+  flex-direction: row;
+
+}
+#logo-content .navlogo .remove img, #logo-content .navlogo .edit img{
+  cursor: pointer;
+  background-color: #ffffff85;
+  border-radius: 4px;
+  margin-left: 8px;
+  padding: 4px;
+  width: auto;
+  height: 22px;
 }
 
-/*add-image */
-.add-image{
-  width: 100%;
+#logo-content .navlogo{
+  position: absolute;
+  z-index: 1;
+  top: 4px;
+  right: 4px;
+}
+ 
+#logo-content {
+  position: relative;
+  border: 1px solid black;
+  border-radius: 4px;
+  width: 16vmax;
+  height: 16vmax;
+}
+
+#logo-content .add-logo {
+  cursor: pointer;
   height: 100%;
+  widows: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 }
 
-/*Image-preview*/
-.image-preview{
-  position: relative;
-  height: 12.8vmax;
-  width: 99%;
+#logo-content .add-logo img{
+  height: 8vmax;
+  width: 8vmax;
+}
+
+#logo-content .preview-logo{
+  height: 100%;
+  width: 100%;
+}
+
+#logo-content .preview-logo img{
+  height: 100%;
+  width: 100%;
+}
+
+/*Logo ends here*/
+
+/*Image-content */
+#image-content{
   display: flex;
-  flex-direction: row;
-  align-items: center;
+  flex-shrink: 0;
+  align-items:center;
+  width: 100%;
+  height: 12vmax;
   overflow-x: auto;
 }
 
-.image-preview .imgs .meta-data img{
-  width: 16px;
-  cursor: pointer;
-  height: auto;
+.image-preview{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin-right: 8px;
+  height: auto;
 }
 
-.image-preview .meta-data{
+.image-preview .image{
   display: flex;
-  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.image-preview .image img {
+  width: 100%;
+  height: 50%;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+
+#top-bar{
+  display: flex;
   align-items: flex-end;
-  justify-content: space-between;
+  width: 8vmax;
   margin-bottom: 4px;
 }
 
-.image-preview .imgs{
- display: flex;
- flex-direction: column;
- margin-left: 16px;
+#top-bar img{
+  height: 20px;
+  cursor: pointer;
+  margin-right: 8px;
+
 }
 
-.image-preview .imgs .meta-data span{
-  width: 8vw;
-  text-align: right;
+#top-bar span{
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
 }
 
-.image-preview .imgs img:nth-child(2n){
-  height: 5vmax;
-  border: 1px solid black;
-  border-radius: 4px;
-  padding: 0.8vmax 0;
-  width: 10vw;
+#image-content .add{
+ border: inherit;
+ background-color: #e7e7e7e7;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ height: 8.2vmax;
+ margin-top: 24px;
 }
 
+#image-content .add span{
+  margin-top: 4px;
+  text-align: center;
+  width: 99%;
+}
 
+#image-content .add span a{
+  color: #00a28c;
+}
+
+.border{
+  border: 1px solid black;
+  border-radius: 4px;
+  width: 8vmax;
+  height: 8vmax;
+}
+
+#image-content .image-preview:nth-child(2) .image::before{
+  content: "Imagem Principal";
+  color: white;
+  position: absolute;
+  text-shadow: 1px 1px 5px black;
+}
 
 
 
@@ -226,6 +379,7 @@ export default {
 
 
 /*Main */
+
 .image input[type="file"]{
   display: none;
 }
@@ -236,7 +390,7 @@ export default {
   padding: 16px;
 }
 
-.card p {
+.card p:first-of-type {
   margin-top: 8px;
   margin-bottom: 16px;
   font-weight: 200;
